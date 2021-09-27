ef315370559daa24863e463a90a6b01aa4cc00b7
Scoutan <johnson.chungck@gmail.com>
2021-09-27 14:51:26 -0400

    Update ItemMenu.js
    added clear button

--- src/components/ItemMenu.js
+++ src/components/ItemMenu.js
@@ -8,22 +8,25 @@ export default function ItemMenu(props) {
       </button>
       <button className="button" onClick={props.save}>
         Save to file
-      </button>
-      <button className="button" onClick={props.clear}>
-        Clear file
-      </button> */}
+      </button>*/}
       <button className="button" onClick={props.test}>
         Fetch Test
       </button>
-      <button className="button" onClick={props.testAPI}>
-        Test API
+      <button className="button" onClick={props.clear}>
+        Clear
       </button>
+      {/* <button className="button" onClick={props.testAPI}>
+        Test API
+      </button> */}
       <ItemFetchBar
         loadingSearch={props.loadingSearch}
         inputValue={props.inputValue}
         inputChange={props.inputChange}
         onFetch={props.onFetch}
       />
+      <div className="examples">
+        Examples: <span>https://www.1999.co.jp/eng/10816807</span> or <span>https://www.amiami.com/eng/detail?gcode=FIGURE-132054</span>
+      </div>
     </div>
   )
 }
